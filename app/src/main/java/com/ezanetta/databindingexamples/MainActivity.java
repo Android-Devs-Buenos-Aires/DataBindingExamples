93f66cf6b2fa45b47e001facd32f974110b039d1
Ezequiel Zanetta <ezequielzanetta@gmail.com>
2016-06-01 20:33:16 -0300

    Custom binding class name added

--- app/src/main/java/com/ezanetta/databindingexamples/MainActivity.java
+++ app/src/main/java/com/ezanetta/databindingexamples/MainActivity.java
@@ -1,18 +1,19 @@
 package com.ezanetta.databindingexamples;
 
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.ezanetta.databindingexamples.Handlers.MainActivityHandlers;
-import com.ezanetta.databindingexamples.databinding.ActivityMainBinding;
+import com.ezanetta.databindingexamples.databinding.MainLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        MainLayout binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setHandlers(new MainActivityHandlers());
     }
 }
+
